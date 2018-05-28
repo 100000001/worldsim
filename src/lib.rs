7c763eecc3de853f88a9129bb276c265b989fef5
Joshua Barretto <joshua.s.barretto@gmail.com>
2018-05-28 17:30:59 +0100

    Added worldgen as dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+extern crate worldgen;
+
+use worldgen::MacroWorld;
+
+pub fn simulate(mw: &mut MacroWorld, ticks: u32) {
+	for i in 0..ticks {
+		println!("A simulation tick has occured.");
+	}
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
