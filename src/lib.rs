322fa37430fee7771ec0309ab434c2b3477d8cae
Joshua Barretto <joshua.s.barretto@gmail.com>
2018-05-28 18:01:00 +0100

    Tabs to spaces

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,9 @@ extern crate worldgen;
 use worldgen::MacroWorld;
 
 pub fn simulate(mw: &mut MacroWorld, ticks: u32) {
-	for i in 0..ticks {
-		println!("A simulation tick has occured.");
-	}
+    for i in 0..ticks {
+        println!("A simulation tick has occured.");
+    }
 }
 
 #[cfg(test)]
